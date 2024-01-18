1ae70191db185b1a7f2db9e367f5f5f8f1dec3b9
guilhermsouza <94068721+guilhermsouza@users.noreply.github.com>
2024-01-18 20:15:07 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+
+const form = document.getElementById('form');
+
+
+form.addEventListener('submit',function(e){
+    let = formEvalido = false;
+    e.preventDefault();
+
+});
+
+const campoA1 = document.getElementById('campoA');
+const campoB1 = document.getElementById('campoB');
+const mensagem = 'O campo B é maior que o campo A!'
+
+
+if(campoA1 == campoB1){
+    alert("ok");
+}
