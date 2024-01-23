429708bc6371f53c9f755b72cb2fe94098b18078
guilhermsouza <94068721+guilhermsouza@users.noreply.github.com>
2024-01-23 12:41:56 -0300

    ALT

--- main.js
+++ main.js
@@ -1,18 +1,23 @@
+const CAMPOA = document.getElementById('CAMPOA');
+const CAMPOB = document.getElementById('CAMPOB');
+var BOTAO = document.getElementById('btn-submit');
 
 const form = document.getElementById('form');
 
 
-form.addEventListener('submit',function(e){
-    let = formEvalido = false;
+
+
+form.addEventListener('submit',function(e) {
     e.preventDefault();
 
-});
+    const mensagemDeSucesso = `Formulário válido, o campo B: ${CAMPOB.value} é MAIOR que o campo A: ${CAMPOA.value}`
+    const mensagemDeErro = `Formulário inválido, o campo B: ${CAMPOB.value} deve ser MAIOR que o campo A: ${CAMPOA.value}`
 
-const campoA1 = document.getElementById('campoA');
-const campoB1 = document.getElementById('campoB');
-const mensagem = 'O campo B é maior que o campo A!'
-
-
-if(campoA1 == campoB1){
-    alert("ok");
+if (CAMPOA.value < CAMPOB.value) {
+    alert(mensagemDeSucesso)
 }
+    else {
+        alert(mensagemDeErro)
+    }
+
+})
